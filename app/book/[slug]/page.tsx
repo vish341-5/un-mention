1f677a25743ebcbdde6ddecd489cdc1f067e4c70
vish341-5 <visheshvishnoi799@gmail.com>
2026-06-09 06:01:10 +0530

    Update page.tsx

--- app/book/[slug]/page.tsx
+++ app/book/[slug]/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { isDayUnlocked, getProgressPercent } from "@/lib/progress";
-
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
