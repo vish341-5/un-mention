519b8d06a70029171b63476443d19b1b34f9089c
vish341-5 <visheshvishnoi799@gmail.com>
2026-06-11 06:10:10 +0530

    Update day2.ts

--- data/books/shoe-dog/day2.ts
+++ data/books/shoe-dog/day2.ts
@@ -1,5 +1,9 @@
-import type { Day } from "@/types/book";
+import { Day } from "@/types/book";
 
-const day2: Day = {};
+const day2: Day = {
+  day: 2,
+  title: "Coming Soon",
+  cards: [],
+};
 
 export default day2;
